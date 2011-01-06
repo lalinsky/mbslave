89cd22a056a74e29f63927c3baa7bc7288bfd38b
Lukáš Lalinský <lalinsky@gmail.com>
2011-01-06 12:14:07 +0100

    The first argument should be the executable name

--- mbslave-psql.py
+++ mbslave-psql.py
@@ -6,7 +6,7 @@ import os
 config = ConfigParser.RawConfigParser()
 config.read(os.path.dirname(__file__) + '/mbslave.conf')
 
-args = []
+args = ['pgsql']
 args.append('-U')
 args.append(config.get('DATABASE', 'user'))
 if config.has_option('DATABASE', 'host'):
